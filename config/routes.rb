0b4fde4e1ee572adf5cef5a6e73b2c4741280f60
neslisahgamze <neslisahgamze@gmail.com>
2018-11-24 22:13:54 +0200

    Improve your design with HTML and CSS

--- config/routes.rb
+++ config/routes.rb
@@ -2,6 +2,7 @@ Rails.application.routes.draw do
   devise_for :users
   resources :comments
   get 'pages/info'
+  get "pages/info"
   root to: redirect('/ideas')
   resources :ideas
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
